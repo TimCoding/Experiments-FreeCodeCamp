3d70d27e9f110fbeafa49fe3714ffc9a7cfcfb9d
Timothy Ho <timbvho@gmail.com>
2016-04-26 03:00:27 -0500

    Update weather.js

--- Weather-App/weather.js
+++ Weather-App/weather.js
@@ -1,9 +1,14 @@
+
 $(document).ready(function(){
+  
+  var currTemp = 0;
+  
   $('#button-weather').click(function(event){
     event.preventDefault();
     var website = "http://api.openweathermap.org/data/2.5/weather?q=";
-    var city = $('#weather-search').val();
-    alert(city);
+    //API only accepts lower case input so...
+    //Will use toLowerCase();
+    var city = $('#weather-search').val().toLowerCase();
     var cityAppend = "?";
     var apiID = "id=524901&APPID=";
     var id = "091cd4a66afe76205aca39cc41f07de1";
@@ -11,8 +16,26 @@ $(document).ready(function(){
     //   $('#display-weather').html(data.main.temp);
     // });
     $.getJSON(website+city+cityAppend+apiID+id, function(data){
-      $('#display-weather').html(data.main.temp);
-
+      currTemp = data.main.temp;
+      $('#display-weather').html(fahrenheit(currTemp));
     });
   });
+  
+  $('#fahrenheit').click(function(){
+    $('#display-weather').html(fahrenheit(currTemp));
+  });
+  
+  $('#celsius').click(function(){
+    $('#display-weather').html(celsius(currTemp));
+  });
 });
+
+function celsius(tempInKelvin){
+  return (tempInKelvin - 273.15) + " degrees Celsius";
+}
+
+function fahrenheit(tempInKelvin){
+  var C = tempInKelvin - 273.15;
+  //Converting from celsius to fahrenheit
+  return ((9.0/5.0) * C + 32) + " degrees Fahrenheit";
+}
